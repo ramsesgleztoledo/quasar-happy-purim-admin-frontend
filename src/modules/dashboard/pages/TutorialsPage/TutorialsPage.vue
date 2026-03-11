3c299a866e2f1e08c6b7f4df3fc83db8c09967c6
ramsesgleztoledo <ramses@3nom.com>
2026-03-11 12:31:02 -0400

    videos fixed

--- src/modules/dashboard/pages/TutorialsPage/TutorialsPage.vue
+++ src/modules/dashboard/pages/TutorialsPage/TutorialsPage.vue
@@ -15,16 +15,20 @@
   <div class="row">
     <div
       class="col-12 col-sm-4 col-md-3 col-lg-3 q-pr-lg q-mb-lg TutorialsPage-col"
-      v-for="tutorial in tutorials"
+      v-for="tutorial in filteredTutorials"
       :key="tutorial.name"
-      :style="{ display: showVideo(tutorial.name).value ? '' : 'none' }"
     >
       <div class="TutorialsPage-video-container">
         <div class="row TutorialsPage-title">
           {{ tutorial.name }}
         </div>
         <div style="position: relative; height: 100%">
-          <q-video :src="tutorial.url" style="position: relative; z-index: 1" />
+          <video
+            :src="tutorial.url"
+            controls
+            preload="metadata"
+            style="width: 100%; height: 100%; position: relative; z-index: 1"
+          ></video>
           <q-inner-loading :showing="true" style="position: absolute">
             <q-spinner size="50px" />
           </q-inner-loading>
@@ -41,8 +45,12 @@ import type { TutorialInterface } from '../../interfaces/tutorials.interface'
 const tutorialService = useTutorialService()
 const tutorials = ref<TutorialInterface[]>([])
 const searchText = ref('')
-const showVideo = (name: string) =>
-  computed(() => name.toLowerCase().includes((searchText.value || '').toLowerCase()))
+
+const filteredTutorials = computed<TutorialInterface[]>(() =>
+  tutorials.value.filter((tutorial) =>
+    tutorial.name.toLowerCase().includes((searchText.value || '').toLowerCase()),
+  ),
+)
 
 onMounted(async () => {
   try {
